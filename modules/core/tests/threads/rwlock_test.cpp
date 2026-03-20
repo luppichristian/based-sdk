04be5790d5b16318a0a9829d2713c4558d7e199a
luppichristian <christian_luppi@outlook.com>
2026-03-20 15:54:27 +0100

    attempting to fix last macos issue

--- modules/core/tests/threads/rwlock_test.cpp
+++ modules/core/tests/threads/rwlock_test.cpp
@@ -11,6 +11,12 @@ namespace {
     atomic_u32* reader_can_proceed;
   } rwlock_writer_ctx;
 
+  typedef struct rwlock_reader_ctx {
+    rwlock lock;
+    atomic_u32* reader_active;
+    atomic_u32* writer_can_proceed;
+  } rwlock_reader_ctx;
+
   func i32 rwlock_writer_entry(void* arg) {
     rwlock_writer_ctx* ctx = (rwlock_writer_ctx*)arg;
     rwlock_write_lock(ctx->lock);
@@ -19,6 +25,17 @@ namespace {
       thread_sleep(1);
     }
     rwlock_write_unlock(ctx->lock);
+    return 0;
+  }
+
+  func i32 rwlock_reader_entry(void* arg) {
+    rwlock_reader_ctx* ctx = (rwlock_reader_ctx*)arg;
+    rwlock_read_lock(ctx->lock);
+    atomic_u32_set(ctx->reader_active, 1);
+    safe_while (atomic_u32_get(ctx->writer_can_proceed) == 0) {
+      thread_sleep(1);
+    }
+    rwlock_read_unlock(ctx->lock);
     return 0;
   }
 
@@ -60,13 +77,22 @@ TEST(threads_rwlock_test, try_read_lock_success) {
 
 TEST(threads_rwlock_test, try_read_lock_fails_with_writer) {
   rwlock lock = rwlock_create();
+  atomic_u32 writer_active = {0};
+  atomic_u32 reader_can_proceed = {0};
+  rwlock_writer_ctx ctx = {lock, &writer_active, &reader_can_proceed};
 
-  rwlock_write_lock(lock);
+  thread writer = thread_create(rwlock_writer_entry, &ctx, (ctx_setup) {0});
+  EXPECT_NE(0, thread_is_valid(writer));
+
+  safe_while (atomic_u32_get(&writer_active) == 0) {
+    thread_sleep(1);
+  }
 
   b32 result = rwlock_try_read_lock(lock);
   EXPECT_EQ(0, result);
 
-  rwlock_write_unlock(lock);
+  atomic_u32_set(&reader_can_proceed, 1);
+  thread_join(writer, NULL);
   EXPECT_NE(0, rwlock_destroy(lock));
 }
 
@@ -82,25 +108,43 @@ TEST(threads_rwlock_test, try_write_lock_success) {
 
 TEST(threads_rwlock_test, try_write_lock_fails_with_reader) {
   rwlock lock = rwlock_create();
+  atomic_u32 reader_active = {0};
+  atomic_u32 writer_can_proceed = {0};
+  rwlock_reader_ctx ctx = {lock, &reader_active, &writer_can_proceed};
 
-  rwlock_read_lock(lock);
+  thread reader = thread_create(rwlock_reader_entry, &ctx, (ctx_setup) {0});
+  EXPECT_NE(0, thread_is_valid(reader));
+
+  safe_while (atomic_u32_get(&reader_active) == 0) {
+    thread_sleep(1);
+  }
 
   b32 result = rwlock_try_write_lock(lock);
   EXPECT_EQ(0, result);
 
-  rwlock_read_unlock(lock);
+  atomic_u32_set(&writer_can_proceed, 1);
+  thread_join(reader, NULL);
   EXPECT_NE(0, rwlock_destroy(lock));
 }
 
 TEST(threads_rwlock_test, try_write_lock_fails_with_writer) {
   rwlock lock = rwlock_create();
+  atomic_u32 writer_active = {0};
+  atomic_u32 reader_can_proceed = {0};
+  rwlock_writer_ctx ctx = {lock, &writer_active, &reader_can_proceed};
 
-  rwlock_write_lock(lock);
+  thread writer = thread_create(rwlock_writer_entry, &ctx, (ctx_setup) {0});
+  EXPECT_NE(0, thread_is_valid(writer));
+
+  safe_while (atomic_u32_get(&writer_active) == 0) {
+    thread_sleep(1);
+  }
 
   b32 result = rwlock_try_write_lock(lock);
   EXPECT_EQ(0, result);
 
-  rwlock_write_unlock(lock);
+  atomic_u32_set(&reader_can_proceed, 1);
+  thread_join(writer, NULL);
   EXPECT_NE(0, rwlock_destroy(lock));
 }
 
